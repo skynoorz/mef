a8e30e3dd8e3ced788aacf201141be94649a3d97
quantum_bolivia <boliviaqtm@gmail.com>
2023-06-14 03:09:05 -0400

    te amo <3

--- src/main/java/com/noraroc/mef/controller/FilterController.java
+++ src/main/java/com/noraroc/mef/controller/FilterController.java
@@ -1,15 +1,24 @@
 package com.noraroc.mef.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.noraroc.mef.dao.TramiteRepository;
+import com.noraroc.mef.entity.Tramite;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class FilterController {
 
+    @Autowired
+    private TramiteRepository tramiteRepository;
+
     @GetMapping("/filter")
     public String filterView(Model model) {
+        List<Tramite> tramites = tramiteRepository.findAll();
+        model.addAttribute("tramites", tramites);
         return "filter";
     }
 }
