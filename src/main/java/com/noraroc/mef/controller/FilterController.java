d96aa46903e7883edf1b7bb854fc939e357ac453
quantum_bolivia <boliviaqtm@gmail.com>
2023-06-18 23:27:03 -0400

    - formulario funcionando

--- src/main/java/com/noraroc/mef/controller/FilterController.java
+++ src/main/java/com/noraroc/mef/controller/FilterController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,6 +22,19 @@ public class FilterController {
     @GetMapping
     public String filterView(Model model) {
         List<Tramite> tramites = tramiteService.findAll();
+        model.addAttribute("tramites", tramites);
+        return "filter";
+    }
+
+    @GetMapping("/form")
+    public String formView(@ModelAttribute Tramite tramite, Model model){
+        model.addAttribute(tramite);
+        return "form_filter";
+    }
+
+    @PostMapping
+    public String procesarFormulario(@ModelAttribute Tramite tramite, Model model) {
+        List<Tramite> tramites = tramiteService.search(tramite);
         model.addAttribute("tramites", tramites);
         return "filter";
     }
